d76d9e204a3812f60dfd1169d358b65a56e1859d
jyotibhokare24 <jyotibhokarecse@gmail.com>
2025-05-21 22:54:28 +0530

    Update ColorSearch.java

--- ListInterface/ColorSearch.java
+++ ListInterface/ColorSearch.java
@@ -1,5 +1,6 @@
-
-
+/*Write a java program for getting different colors through ArrayList interface and search whether
+*the color "Red" is available or not 
+*/
 package arryalist;
 import java.util.ArrayList;
 import java.util.List;
